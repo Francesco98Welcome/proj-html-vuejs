1416a3d44b208d65d504e7cbe5bac43179fcaaac
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-02-13 12:53:41 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+//import { store } from './store.js';
+import AppHeader from './components/Header/AppHeader.vue';
+import AppMain from './components/Main/AppMain.vue';
+import AppFooter from './components/Footer/AppFooter.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data() {
+    return {
+    }
+  },
+
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
+</template>
+
+<style lang="scss">
+@import './styles/main.scss';
+</style>
